4dbe4b8daab3ccf38ad1883dc2b51ce952739edb
kuldarvakker <kuvakk@taltech.ee>
2023-04-24 06:04:31 +0300

    JavaFluent test

--- src/main/kotlin/ee/taltech/crossfieldvalidation/java_fluent/JavaFluentController.kt
+++ src/main/kotlin/ee/taltech/crossfieldvalidation/java_fluent/JavaFluentController.kt
@@ -1,20 +1,39 @@
 package ee.taltech.crossfieldvalidation.java_fluent
 
+import br.com.fluentvalidator.Validator
+import ee.taltech.crossfieldvalidation.ValidationError
+import ee.taltech.crossfieldvalidation.ValidationErrors
 import ee.taltech.crossfieldvalidation.java_fluent.model.Person
-import jakarta.validation.Valid
+import ee.taltech.crossfieldvalidation.java_fluent.model.PersonType
+import ee.taltech.crossfieldvalidation.java_fluent.model.PrivatePerson
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class JavaFluentController {
 
+    private final val privatePersonValidator: Validator<PrivatePerson> = PrivatePersonValidator()
+
     @PostMapping("/api/java_fluent")
-    fun validatePerson(@Valid @RequestBody person: Person): ResponseEntity<Person> {
+    fun validatePerson(@RequestBody person: Person): ResponseEntity<*> {
+        val results = when (person.type) {
+            PersonType.PRIVATE -> privatePersonValidator.validate(person as PrivatePerson)
+            PersonType.COMPANY -> privatePersonValidator.validate(person as PrivatePerson)
+        }
+        if (!results.isValid) {
+            val errors = results.errors.map {
+                ValidationError(
+                    field =it.field ?: "null code",
+                    message = it.message
+                )
+            }
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ValidationErrors(errors))
+        }
 
         return ResponseEntity.status(HttpStatus.OK).body(person)
-    }
 
+    }
 }
---
+++ src/test/kotlin/ee/taltech/crossfieldvalidation/java_fluent/JavaFluentControllerTest.kt
@@ -0,0 +1,71 @@
+package ee.taltech.crossfieldvalidation.java_fluent
+
+import ee.taltech.crossfieldvalidation.PersonJson
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.post
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class JavaFluentControllerTest(@Autowired private val mockMvc: MockMvc) {
+
+    private val url = "/api/java_fluent"
+
+    @Test
+    fun `validatePerson - success`() {
+        mockMvc.post(url) {
+            contentType = MediaType.APPLICATION_JSON
+            content = PersonJson.validPerson
+            accept = MediaType.APPLICATION_JSON
+        }.andExpect {
+            status { isOk() }
+            content { json(PersonJson.validPerson, strict = true) }
+        }
+    }
+
+    @Test
+    fun `validatePerson - failure`() {
+        mockMvc.post(url) {
+            contentType = MediaType.APPLICATION_JSON
+            content = PersonJson.invalidPerson
+            accept = MediaType.APPLICATION_JSON
+        }.andExpect {
+            status { isBadRequest() }
+            content { json("""
+                {
+                  "errors": [
+                    {
+                      "field": "address.city",
+                      "message": "size must be at greater or equal to 2"
+                    },
+                    {
+                      "field": "firstName",
+                      "message": "size must be between 2 and 4"
+                    },
+                    {
+                      "field": "atLeastPhoneOrEmailPresent",
+                      "message": "Phone or Email must be present"
+                    },
+                    {
+                      "field": "height.value",
+                      "message": "numeric value out of bounds (<3 digits>.<2 digits> expected)"
+                    },
+                    {
+                      "field": "weight.value",
+                      "message": "numeric value out of bounds (<3 digits>.<2 digits> expected)"
+                    },
+                    {
+                      "field": "address.street",
+                      "message": "size must be at greater or equal to 2"
+                    }
+                  ]
+                }
+            """.trimIndent())
+            }
+        }
+    }
+}
