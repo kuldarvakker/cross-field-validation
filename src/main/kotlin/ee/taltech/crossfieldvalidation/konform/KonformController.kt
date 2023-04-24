a5f86fac84cbd83ead586575182f6095a389d4d4
kuldarvakker <kuvakk@taltech.ee>
2023-04-24 21:26:15 +0300

    konform

--- src/main/kotlin/ee/taltech/crossfieldvalidation/konform/KonformController.kt
+++ src/main/kotlin/ee/taltech/crossfieldvalidation/konform/KonformController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class KonformController {
 
-    @PostMapping("/api/ktor")
+    @PostMapping("/api/konform")
     fun validatePerson(@Valid @RequestBody person: Person): ResponseEntity<Person> {
 
         return ResponseEntity.status(HttpStatus.OK).body(person)
