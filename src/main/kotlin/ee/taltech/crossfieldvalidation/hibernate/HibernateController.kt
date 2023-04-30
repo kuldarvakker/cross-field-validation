e826c7ea5f37321be3301c06d911ac567d68ff13
kuldarvakker <kuvakk@taltech.ee>
2023-05-01 02:18:46 +0300

    hibernate simplify, use validator obj

--- src/main/kotlin/ee/taltech/crossfieldvalidation/hibernate/HibernateController.kt
+++ src/main/kotlin/ee/taltech/crossfieldvalidation/hibernate/HibernateController.kt
@@ -1,24 +1,34 @@
 package ee.taltech.crossfieldvalidation.hibernate
 
+import ee.taltech.crossfieldvalidation.ValidationError
+import ee.taltech.crossfieldvalidation.ValidationErrors
 import ee.taltech.crossfieldvalidation.hibernate.model.Person
-import jakarta.validation.Valid
 import jakarta.validation.Validation
 import jakarta.validation.Validator
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.PostMapping
 
 @RestController
-@Validated
 class HibernateController {
 
     private val validator = getValidator()
 
     @PostMapping("/api/hibernate")
-    fun validatePerson(@Valid @RequestBody person: Person): ResponseEntity<*> {
+    fun validatePerson(@RequestBody person: Person): ResponseEntity<*> {
+        val results = validator.validate(person)
+        if (results.isNotEmpty()) {
+            val errors = results.map {
+                ValidationError(
+                    field =it.propertyPath.toString(),
+                    message = it.message
+                )
+            }
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ValidationErrors(errors))
+        }
+
         return ResponseEntity.status(HttpStatus.OK).body(person)
     }
 
--- src/main/kotlin/ee/taltech/crossfieldvalidation/hibernate/model/Person.kt
+++ src/main/kotlin/ee/taltech/crossfieldvalidation/hibernate/model/Person.kt
@@ -2,9 +2,8 @@ package ee.taltech.crossfieldvalidation.hibernate.model
 
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
-import org.springframework.validation.annotation.Validated
 
-@Validated
+
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "type")
 @JsonSubTypes(
     JsonSubTypes.Type(value = PrivatePerson::class, name = "PRIVATE"),
