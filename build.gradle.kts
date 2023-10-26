5c2d4ff7c42466a653bfd5ca79b81a308952c5ef
kuldarvakker <kuvakk@taltech.ee>
2023-10-26 18:02:05 +0300

    add /v3/api-docs

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ extra["snippetsDir"] = file("build/generated-snippets")
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
