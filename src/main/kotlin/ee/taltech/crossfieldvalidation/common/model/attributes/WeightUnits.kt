da72c67ce2702a05845dd41ac612a1c9295e2ad6
kuldarvakker <kuvakk@taltech.ee>
2023-11-02 20:33:51 +0200

    common model height weight units

--- src/main/kotlin/ee/taltech/crossfieldvalidation/common/model/attributes/WeightUnits.kt
+++ src/main/kotlin/ee/taltech/crossfieldvalidation/common/model/attributes/WeightUnits.kt
@@ -1,7 +1,9 @@
 package ee.taltech.crossfieldvalidation.common.model.attributes
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import io.swagger.v3.oas.annotations.media.Schema
 
+@Schema(type = "String", description = "Height's unit", allowableValues = [])
 enum class WeightUnits {
     @JsonProperty(value = "kg")
     KG
