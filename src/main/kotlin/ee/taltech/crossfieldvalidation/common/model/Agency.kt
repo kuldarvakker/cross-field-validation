2c37813060f7157ec9e961b56a883282630fc8be
kuldarvakker <kuvakk@taltech.ee>
2023-11-02 12:06:25 +0200

    Agency desc api doc

--- src/main/kotlin/ee/taltech/crossfieldvalidation/common/model/Agency.kt
+++ src/main/kotlin/ee/taltech/crossfieldvalidation/common/model/Agency.kt
@@ -2,7 +2,7 @@ package ee.taltech.crossfieldvalidation.common.model
 
 import io.swagger.v3.oas.annotations.media.Schema
 
-@Schema(type = "String", description = "Provider of the service", allowableValues = [])
+@Schema(type = "String", description = "Agency to be applied for", allowableValues = [])
 enum class Agency {
     GENERAL,
     COMPANY_A,
