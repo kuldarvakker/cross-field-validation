8f3eb5f5e9cf5dde1e06c7c666513727474499c4
kuldarvakker <kuvakk@taltech.ee>
2023-11-02 12:05:40 +0200

    Agency.GENERAL

--- src/main/kotlin/ee/taltech/crossfieldvalidation/common/model/Agency.kt
+++ src/main/kotlin/ee/taltech/crossfieldvalidation/common/model/Agency.kt
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(type = "String", description = "Provider of the service", allowableValues = [])
 enum class Agency {
+    GENERAL,
     COMPANY_A,
     COMPANY_B
 }
