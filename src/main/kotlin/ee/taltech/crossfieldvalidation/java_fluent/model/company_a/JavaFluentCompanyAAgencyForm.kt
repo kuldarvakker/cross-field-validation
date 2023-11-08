3cfedad036c4d1b203758b35d9d206ebd5e8fd20
kuldarvakker <kuvakk@taltech.ee>
2023-11-08 17:08:39 +0200

    JavaFluent schema A,B

--- src/main/kotlin/ee/taltech/crossfieldvalidation/java_fluent/model/company_a/JavaFluentCompanyAAgencyForm.kt
+++ src/main/kotlin/ee/taltech/crossfieldvalidation/java_fluent/model/company_a/JavaFluentCompanyAAgencyForm.kt
@@ -8,12 +8,14 @@ import java.time.LocalDate
 data class JavaFluentCompanyAAgencyForm(
     @field:Schema(allowableValues = ["COMPANY_A"])
     override val agency: Agency = Agency.COMPANY_A,
+    @field:Schema(minLength = 1, maxLength = 128)
     override val firstName: String,
+    @field:Schema(minLength = 1, maxLength = 128)
     override val lastName: String,
     @field:Schema(description = "birthDate must be equal or after 2000")
     override val birthDate: LocalDate,
-    @field:Schema(description = "phoneNumber or email must be present", example = "+372123456789")
+    @field:Schema(minLength = 5, maxLength = 35, description = "phoneNumber or email must be present", example = "+372123456789")
     override val phoneNumber: String?,
-    @field:Schema(description = "phoneNumber or email must be present", example = "email@email.ee")
+    @field:Schema(minLength = 1, maxLength = 128, description = "phoneNumber or email must be present", example = "email@email.ee")
     override val email: String?,
 ) : JavaFluentAgencyForm()
--- src/main/kotlin/ee/taltech/crossfieldvalidation/java_fluent/model/company_b/JavaFluentCompanyBSocialMedia.kt
+++ src/main/kotlin/ee/taltech/crossfieldvalidation/java_fluent/model/company_b/JavaFluentCompanyBSocialMedia.kt
@@ -7,5 +7,6 @@ import io.swagger.v3.oas.annotations.media.Schema
 data class JavaFluentCompanyBSocialMedia(
     @field:Schema(allowableValues = ["FACEBOOK", "TWITTER_X"])
     override val platform: SocialMediaPlatforms,
+    @field:Schema(minLength = 1, maxLength = 128)
     override val profileUrl: String
 ): SocialMedia()
