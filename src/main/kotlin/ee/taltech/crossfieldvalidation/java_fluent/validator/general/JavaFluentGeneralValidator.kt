b2d407d7840780b6a5305f9d8b6de5abf08e45dc
kuldarvakker <kuvakk@taltech.ee>
2023-11-10 11:21:43 +0200

    add todo to fluent

--- src/main/kotlin/ee/taltech/crossfieldvalidation/java_fluent/validator/general/JavaFluentGeneralValidator.kt
+++ src/main/kotlin/ee/taltech/crossfieldvalidation/java_fluent/validator/general/JavaFluentGeneralValidator.kt
@@ -12,20 +12,6 @@ class JavaFluentGeneralValidator : FluentValidator<JavaFluentGeneralAgencyForm>(
 
     override fun rules() {
         setPropertyOnContext("GENERAL")
-        ruleFor(JavaFluentGeneralAgencyForm::firstName)
-            .must(stringSizeBetween(1, 128))
-            .`when`(not(nullValue()))
-            .withMessage("Size 1, 128")
-            // .withFieldName()
-        ruleFor(JavaFluentGeneralAgencyForm::lastName)
-            .must(stringSizeBetween(1, 128))
-            .`when`(not(nullValue()))
-            .withMessage("Size 1, 128")
-        // .withFieldName()
-        ruleFor(JavaFluentGeneralAgencyForm::birthDate)
-            .must(localDateAfterOrEqual(LocalDate.ofYearDay(2000, 1)))
-            .`when`(not(nullValue()))
-            .withMessage("birthDate must be equal or after 2000")
-        // .withFieldName()
+        TODO("finish this validation")
     }
 }
