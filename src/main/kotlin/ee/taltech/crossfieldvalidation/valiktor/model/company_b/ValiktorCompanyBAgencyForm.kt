5d64d92d280f6f9f4556665cd4dcea584da5a29f
kuldarvakker <kuvakk@taltech.ee>
2023-11-28 01:26:31 +0200

    Valiktor v1

--- src/main/kotlin/ee/taltech/crossfieldvalidation/valiktor/model/company_b/ValiktorCompanyBAgencyForm.kt
+++ src/main/kotlin/ee/taltech/crossfieldvalidation/valiktor/model/company_b/ValiktorCompanyBAgencyForm.kt
@@ -1,10 +1,21 @@
 package ee.taltech.crossfieldvalidation.valiktor.model.company_b
 
+import ee.taltech.crossfieldvalidation.checkNumericValueBounds
 import ee.taltech.crossfieldvalidation.common.model.Agency
 import ee.taltech.crossfieldvalidation.common.model.attributes.Gender
+import ee.taltech.crossfieldvalidation.common.model.attributes.HeightUnits
+import ee.taltech.crossfieldvalidation.common.model.attributes.SocialMediaPlatforms.FACEBOOK
+import ee.taltech.crossfieldvalidation.common.model.attributes.SocialMediaPlatforms.TWITTER_X
 import ee.taltech.crossfieldvalidation.valiktor.model.ValiktorAgencyForm
+import ee.taltech.crossfieldvalidation.valiktor.withMessage
 import io.swagger.v3.oas.annotations.media.ArraySchema
 import io.swagger.v3.oas.annotations.media.Schema
+import org.valiktor.functions.hasSize
+import org.valiktor.functions.isIn
+import org.valiktor.functions.isValid
+import org.valiktor.functions.validate
+import org.valiktor.functions.validateForEach
+import org.valiktor.validate
 
 data class ValiktorCompanyBAgencyForm(
     @field:Schema(allowableValues = ["COMPANY_B"])
@@ -18,4 +29,24 @@ data class ValiktorCompanyBAgencyForm(
     @field:ArraySchema(minItems = 1, maxItems = 2)
     override val socialMedia: List<ValiktorCompanyBSocialMedia>,
     override val height: ValiktorCompanyBHeight,
-) : ValiktorAgencyForm()
+) : ValiktorAgencyForm() {
+
+    init {
+        validate(this) { _ ->
+            validate(ValiktorCompanyBAgencyForm::agency).isValid { it == Agency.COMPANY_B }
+            validate(ValiktorCompanyBAgencyForm::firstName).hasSize(min = 1, max = 50)
+            validate(ValiktorCompanyBAgencyForm::lastName).hasSize(min = 1, max = 50)
+            validate(ValiktorCompanyBAgencyForm::gender).isIn(Gender.MALE)
+            validate(ValiktorCompanyBAgencyForm::socialMedia).hasSize(1,2).validateForEach {
+                validate(ValiktorCompanyBSocialMedia::platform).isIn(FACEBOOK, TWITTER_X)
+                validate(ValiktorCompanyBSocialMedia::profileUrl).hasSize(1, 128)
+            }
+            validate(ValiktorCompanyBAgencyForm::height).validate { height ->
+                validate(ValiktorCompanyBHeight::unit).isIn(HeightUnits.CM)
+                validate(ValiktorCompanyBHeight::value).withMessage("numeric value out of bounds (<3 digits>.<0 digits> expected)") {
+                    checkNumericValueBounds(height.value, 3, 2)
+                }
+            }
+        }
+    }
+}
